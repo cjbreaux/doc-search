70fbad19b96c17623e3c60ccd02b2a10a49a3ae2
Chris Breaux <christopherjbreaux@gmail.com>
2019-03-22 12:48:24 -0700

    fix api call to include Portland

--- src/js/apiLogic.js
+++ src/js/apiLogic.js
@@ -2,7 +2,25 @@ export class DoctorSearch {
   findDoctor(name) {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&user_key=3b3ad342c89b258cc5e38ed6df125671&limit=10`;
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&location=or-portland&user_location=45.5122%2C%20-122.6587&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
+}
+
+export class ConditionSearch {
+  findCondition(condition) {
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      let url = `https://api.betterdoctor.com/2016-03-01/conditions?fields=${condition}&user_key=${process.env.exports.apiKey}`;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 import $ from 'jquery';
 import './scss/styles.scss';
 import { DoctorSearch } from './js/apiLogic.js';
+import { ConditionSearch } from './js/apiLogic.js';
 
+function sorter(obj) {
+  obj.data.forEach(function(doc){
+    return {'name': doc.practices[0].name}
+  })
+}
 
 $(document).ready(function(){
   $('form').submit(function(event){
@@ -13,15 +19,28 @@ $(document).ready(function(){
 
 
     const doc = new DoctorSearch();
-    const promise = doc.findDoctor();
+    const promise = doc.findDoctor(doctorInput);
 
     promise.then(function(response) {
       const results = JSON.parse(response);
-      console.log(results);
+      // console.log(results);
+      const resultsArr = results;
+      // const output = sorter(results);
+      console.log(resultsArr.data);
+
       }, function(error) {
         console.log("Error from API")
       });
 
+    //   const cond = new ConditionSearch();
+    //   const promise2 = cond.findCondition(ailmentInput);
+    //
+    // promise2.then(function(response) {
+    //   const results = JSON.parse(response);
+    //   console.log(results);
+    //   }, function(error) {
+    //     console.log("Error from API")
+    //   });
   })
 
 });
