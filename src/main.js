833148b7fbea77ff41c77c536e13ddfdf3b49855
Chris Breaux <christopherjbreaux@gmail.com>
2019-03-22 13:24:59 -0700

    extract name, number, address, etc

--- src/main.js
+++ src/main.js
@@ -3,11 +3,10 @@ import './scss/styles.scss';
 import { DoctorSearch } from './js/apiLogic.js';
 import { ConditionSearch } from './js/apiLogic.js';
 
-function sorter(obj) {
-  obj.data.forEach(function(doc){
-    return {'name': doc.practices[0].name}
-  })
-}
+// function sorter(obj) {
+//   for (let i= 0; i<obj.practices.length; i++) {
+//   }
+// }
 
 $(document).ready(function(){
   $('form').submit(function(event){
@@ -24,6 +23,13 @@ $(document).ready(function(){
       const results = JSON.parse(response);
       const resultsArr = results;
       console.log(resultsArr.data[0].practices[0].name);
+      console.log(resultsArr.data[0].practices[0].phones[0].number);
+      console.log(resultsArr.data[0].practices[0].visit_address.street);
+      console.log(resultsArr.data[0].practices[0].visit_address.street2);
+      console.log(resultsArr.data[0].practices[0].visit_address.city);
+      console.log(resultsArr.data[0].practices[0].visit_address.state);
+      console.log(resultsArr.data[0].practices[0].visit_address.zip);
+      console.log(resultsArr.data[0].practices[0].accepts_new_patients);
       // const output = sorter(results);
       // console.log(output);
 
