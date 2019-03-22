cb4505bfcd7c51bea8f3b9377e5bdb3b4ad31e1b
Chris Breaux <christopherjbreaux@gmail.com>
2019-03-22 11:24:53 -0700

    contains working API call with hard-coded KEY

--- src/js/apiLogic.js
+++ src/js/apiLogic.js
@@ -1,21 +1,17 @@
 export class DoctorSearch {
-
-  findDoctor(searchTerm, apiKey) {
-    return function(docName) {
-      return new Promise(function(resolveFunction, rejectFunction) {
-        let request = new XMLHttpRequest();
-        let url = `https://api.betterdoctor.com/2016-03-01/doctors?${searchTerm}=${docName}&location=45.5122%2C-122.6587&user_location=45.5122%2C-122.6587&skip=0&user_key=${apiKey}`
-
-        request.onload = function() {
-          if (this.status === 200) {
-            resolveFunction(request.response);
-          } else {
-            rejectFunction(Error(request.statusText));
-          }
+  findDoctor(name) {
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&user_key=3b3ad342c89b258cc5e38ed6df125671&limit=10`;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
         }
-
-        request.open("GET", url, true);
-        request.send();
-      });
-    }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
   }
+}
--- src/main.js
+++ src/main.js
@@ -1,11 +1,27 @@
 import $ from 'jquery';
 import './scss/styles.scss';
-// import { DoctorSearch } from './js/apiLogic.js';
+import { DoctorSearch } from './js/apiLogic.js';
 
 
 $(document).ready(function(){
   $('form').submit(function(event){
     event.preventDefault();
-    console.log("test");
+    let doctorInput = $('#doctor').val();
+    let ailmentInput = $('#ailments').val();
+    console.log(doctorInput);
+    console.log(ailmentInput);
+
+
+    const doc = new DoctorSearch();
+    const promise = doc.findDoctor();
+
+    promise.then(function(response) {
+      const results = JSON.parse(response);
+      console.log(results);
+      }, function(error) {
+        console.log("Error from API")
+      });
+
   })
+
 });
